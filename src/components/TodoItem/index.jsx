31dddbe9009db97b6a4ed1213e0e4a02f3220816
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-09-08 19:40:06 -0300

    Simplificação as funções

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,10 +2,6 @@ import styles from "./todoItem.module.css"
 
 export default function TodoItem(props) {
     return (
-        <tr>
-                <td className={styles.center}>{props.id}</td>
-                <td>{props.tarefa}</td>
-                <td>{props.data}</td>
-        </tr>
+        <li>{props.task}</li>
     )
 }
